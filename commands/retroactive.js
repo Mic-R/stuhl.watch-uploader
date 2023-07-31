ec9d1c9ac46ffcdbf106530fafd51e39f39f14eb
Michael "Mischa Alexandrowitsch" Reimchen <hi@mic-r.eu>
2023-07-31 19:53:31 +0200

    do not review my code it will cause headaches and I will not pay your medical bill

--- commands/retroactive.js
+++ commands/retroactive.js
@@ -13,7 +13,10 @@ module.exports = {
                 return;
             }
 
+            console.log(messagesColl.size);
+            console.log("--- RETROACTIVE UPLOAD ---");
             messagesColl.forEach(async (message) => {
+                console.log(message.id);
                 let search = await prisma.message.findFirst({ where: { id: message.id } });
                 if(search) return;
 
@@ -29,6 +32,8 @@ module.exports = {
             });
 
             interaction.reply("Retroactive upload complete", {ephemeral: true});
+
+            console.log("--- RETROACTIVE UPLOAD COMPLETE ---");
         });
     }
 }
