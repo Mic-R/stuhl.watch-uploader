e949b640379834c44d88da3180e16064790b36aa
Michael "Mischa" Alexandrowitsch R <hi@mic-r.eu>
2023-08-12 22:14:26 +0200

    Update uploadToAzure.js

--- func/uploadToAzure.js
+++ func/uploadToAzure.js
@@ -1,25 +1,20 @@
 module.exports = async (message, containerClient) => {
     const websiteUpdate = require("./websiteUpdate.js");
     if (message.partial) await message.fetch()
-    const attachments = message.attachments;
-
-    let i = 1;
-
-    console.log(attachments);
-    for (const [key, value] of attachments) {
-        console.log("Upload");
-        const fileType = value.contentType.split("/")[1];
-        const blobClient = containerClient.getBlobClient(`${Date.now()}/${message.id}-${i++}.${fileType}`);
-        const blockBlobClient = blobClient.getBlockBlobClient();
-        const response = await fetch(value.url);
-        const buffer = await response.arrayBuffer();
-        const uploadBlobResponse = await blockBlobClient.upload(buffer, buffer.byteLength, {
-            tags: {
-                'messageid': message.id.toString()
-            }
-        });
-        console.log(`Upload block blob successfully`, uploadBlobResponse.requestId, key);
-    }
+    const attachment = message.content;
+    
+    console.log("Upload");
+    const fileType = attachment.substring(attachment.lastIndexOf("."));
+    const blobClient = containerClient.getBlobClient(`${Date.now()}/${message.id}${fileType}`);
+    const blockBlobClient = blobClient.getBlockBlobClient();
+    const response = await fetch(attachment);
+    const buffer = await response.arrayBuffer();
+    const uploadBlobResponse = await blockBlobClient.upload(buffer, buffer.byteLength, {
+        tags: {
+            'messageid': message.id.toString()
+        }
+    });
+    console.log(`Upload block blob successfully`, uploadBlobResponse.requestId, key);
 
     await websiteUpdate(containerClient);
 }
